144f39fc0647f70a9d5ebb2a20cda6e4097cef47
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2023-04-18 15:13:46 +0200

    Initial commit

--- src/Methods.java
+++ src/Methods.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Methods {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Methods {
+    public static ArrayList<String> readFile() {
+        ArrayList<String> List = new ArrayList<String>();
+        Scanner fileReader = null;
+        try {
+            fileReader = new Scanner(new File("ideaold.log"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (fileReader.hasNext()) {
+            List.add(fileReader.nextLine());
+        }
+
+        return List;
+    }
+
+    public static int countErrors(ArrayList<String> theList) {
+        int amountOfErrors = 0;
+
+        for(int i = 0; i < theList.size(); i++) {
+
+        }
+
+        return 0;
+    }
 }
+
+
+
