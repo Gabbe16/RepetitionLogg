3bf4e2717789660a0a2ab4f79acf3a69aa4acd44
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2023-04-21 11:08:37 +0200

    Del 2 och tre klar med påbörjan av 4:an

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
         ArrayList<String> logFile = Methods.readFile();
-        Methods.countErrors(logFile);
+
+        int amountOfErrors = Methods.countErrors(logFile);
+        System.out.println("There are " + amountOfErrors + " Errors" + "\n");
+
+        System.out.println("Here is the gitActivity: ");
+        ArrayList<String> gitActivity = Methods.getGitActivity(logFile);
+
 
     }
 }
